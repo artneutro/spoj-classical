5800adc37f9ddf16e74d316d22f3ff488e070ad2
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2023-10-20 18:21:23 +0100

    TEST - Life, the Universe, and Everything

---
+++ 1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#
+# Author: Jose Lo Huang
+# Creation Date: 20/10/2023
+# Updates:
+# 
+# SPOJ
+# Problem 1
+# TEST - Life, the Universe, and Everything
+# https://www.spoj.com/problems/TEST/
+#
+
+#
+# Print the input until it finds 42
+#
+
+while True :
+    value = input()
+    if int(value) == 42 :
+        break ;
+    print(value)
+
+
+
+
+
+
